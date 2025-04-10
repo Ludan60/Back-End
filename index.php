0d6f70e67d71e9a35cc33d9508e0ba4293cda24a
Ludan60 <ludanahmed7@gmail.com>
2025-04-10 05:54:14 -0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,22 @@
+<?php
+// Q1
+$number1 = 10;
+$number2 = 20;
+$sum = $number1 + $number2;
+
+echo "The sum of $number1 and $number2 is: $sum";
+
+// Q2
+$a = 4;
+echo "<hr> I have " . ++$a . " books<br>"; 
+echo "I have " . $a++ . " books<br>";
+ 
+// Q3
+
+$sentence = null;
+$first = "In";
+$second = "PHP";
+$third = "Course";
+echo  "<hr>".$first.$sentence.$second.$third;
+
+?>
